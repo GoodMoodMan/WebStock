7c89a3e0d3842afd27e7b37e083784775eedb28b
GoodMoodMan <86624574+GoodMoodMan@users.noreply.github.com>
2024-08-09 14:25:28 +0300

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const app = express();
+const User = require('./models/User');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const mongoURI = "mongodb+srv://test:ljR36wHjImUdTaad@cluster0.flnm2su.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// login verification
+app.post('/users/login', async (req, res) => {
+  if(req.method === 'OPTIONS') {
+    return res.status(200).json({ body: "OK" });
+  }
+
+  const { username, password } = req.body;
+  console.log(username);
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+
+    // Remove password from user object before sending
+    const userResponse = user.toObject();
+    delete userResponse.password;
+    res.json(userResponse);
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// signup user post
+app.post('/users/signup', async (req, res) => {
+  if(req.method === 'OPTIONS') {
+    return res.status(200).json({ body: "OK" });
+  }
+
+  const { username, email, password } = req.body;
+  console.log(username);
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(401).json({ error: 'Username Taken' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username: username,
+      password: hashedPassword,
+      email: email,
+      admin: false
+    });
+
+    await newUser.save();
+    return res.status(200).send('Signup Successful');
+  } catch (error) {
+    console.error('Error signing up user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+module.exports = app;
